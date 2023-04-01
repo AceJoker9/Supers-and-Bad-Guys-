75003b9cc2b89e8d93f462a3f61ee44db875b2c2
unknown <neilmagic25@gmail.com>
2023-04-01 00:55:15 -0400

    make the migrations, made the adjustments within the settings file

--- supers_project/settings.py
+++ supers_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*1mj1)gam^-wed9j-zwc9^w7^@_l8$a_k#)=psui_5xj4(v(sf'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,15 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'supers_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +111,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try: 
+    from supers_project.local_settings import *
+except ImportError: 
+    pass 
+
